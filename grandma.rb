d0f9fb2668205ccb9f286803087599432e6c57c2
Cody Dupuis <codydupuis99@gmail.com>
2020-04-17 01:52:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,6 @@ def speak_to_grandma
   if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     elsif speak == "I SAID I LOVE YOU!"
-    puts ""
+    puts "NO, NOT SINCE 1938!"
+  els
     
