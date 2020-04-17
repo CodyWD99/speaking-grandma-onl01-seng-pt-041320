e31d3793754ec1807a80d823028812459fa4160a
Cody Dupuis <codydupuis99@gmail.com>
2020-04-17 02:21:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(talk)
   if "#{talk}" == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif "#{talk}" == .upcase
+  elsif "#{talk}" == "#{talk}".upcase
     puts "NO, NOT SINCE 1938!"
     "NO, NOT SINCE 1938!"
   else
