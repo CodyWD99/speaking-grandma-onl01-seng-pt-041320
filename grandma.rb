d65c61c2ba9967da9ce5d4bb4b7bd29e9bd6af96
Cody Dupuis <codydupuis99@gmail.com>
2020-04-17 02:16:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(talk)
   if "#{talk}" == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif "#{talk}" == "akjsdngkjangf".upcase
+  elsif "#{talk}" == "".upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
